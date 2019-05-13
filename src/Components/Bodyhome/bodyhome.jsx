478ab76bbea729de045cf7888d675edefa532a3b
musyafa619 <musyafaarts@gmail.com>
2019-05-13 14:58:17 +0700

    dashboard

--- src/Components/Bodyhome/bodyhome.jsx
+++ src/Components/Bodyhome/bodyhome.jsx
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
   root: {
@@ -19,18 +22,19 @@ function Bodyhome(props) {
   const { classes } = props;
 
   return (
-    <div className={classes.root} style={{ padding: 50 }}>
+    <div className={classes.root} align="center">
       <Grid container spacing={24}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <img src="https://mm-image-marketing.s3.amazonaws.com/mds/banner/2019/MAY/120519/DESKTOP_A1_MENWOMEN.gif" />
-          </Paper>
+          <img src="https://i.ibb.co/bv5dZyV/Untitled.png" />
+        </Grid>
+        <Grid item xs={12}>
+          <img src="https://mm-image-marketing.s3.amazonaws.com/mds/banner/2019/MAY/120519/DESKTOP_A1_MENWOMEN.gif" />
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>
             <img
               src="https://mm-image-marketing.s3.amazonaws.com/mds/banner/2019/MAY/060519/DESKTOP_B1_MEN-03.jpg"
-              style={{ maxHeight: 500 }}
+              style={{ maxHeight: 490 }}
             />
           </Paper>
         </Grid>
@@ -38,11 +42,12 @@ function Bodyhome(props) {
           <Paper className={classes.paper}>
             <img
               src="https://mm-image-marketing.s3.amazonaws.com/mds/banner/2019/MAY/060519/DESKTOP_B2_MEN-04.jpg"
-              style={{ maxHeight: 500 }}
+              style={{ maxHeight: 490 }}
             />
           </Paper>
         </Grid>
       </Grid>
+      <br />
       <div align="center">
         <h1>
           <strong>PRODUK TERBARU</strong>
@@ -105,6 +110,68 @@ function Bodyhome(props) {
           </Grid>
         </Grid>
       </Grid>
+      <br />
+      <br />
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <img
+            src="https://mm-image-marketing.s3.amazonaws.com/mds/banner/2019/MAY/130519/desktop_c_361.jpg"
+            style={{ maxHeight: 500 }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <img
+            src="https://mm-image-marketing.s3.amazonaws.com/mds/banner/DISNEY_AVENGERS_END_GAME/C4_DISNEY_AVENGER.jpg"
+            style={{ maxHeight: 500 }}
+          />
+        </Grid>
+      </Grid>
+      <div align="center">
+        <h1>
+          <strong>Populer Brands</strong>
+        </h1>
+      </div>
+
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Grid item xs={12}>
+            <Grid container justify="center" spacing="16">
+              <Grid item xs={3}>
+                <img
+                  src="http://eleva.co/wp-content/uploads/2018/09/UAsbb6wU_400x400.jpeg"
+                  style={{ maxHeight: 130 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <img
+                  src="https://mm-image-marketing.s3.amazonaws.com/mds/bangsawan/MEN/CARDINAL.jpg"
+                  style={{ maxWidth: 200 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <img
+                  src="https://mm-image-marketing.s3.amazonaws.com/mds/bangsawan/MEN/LOIS.jpg"
+                  style={{ maxWidth: 200 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <img
+                  src="https://mm-image-marketing.s3.amazonaws.com/mds/bangsawan/MEN/YONGKI_KOMALADI.jpg"
+                  style={{ maxWidth: 200 }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Toolbar>
+        <br />
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <img src="https://i.ibb.co/PM948L5/metode-pembayaran.png" />
+            </Paper>
+          </Grid>
+        </Grid>
+      </AppBar>
     </div>
   );
 }
